4f5a11c0e309434598b27b22a0db9a76a4b5e02c
shas9 <53976819+shas9@users.noreply.github.com>
2020-03-16 12:57:11 +0600

    Update ncr.cpp

--- ncr.cpp
+++ ncr.cpp
@@ -19,3 +19,44 @@ ll nCr(ll n, ll k )
 
     return (x / y) % mod;
 }
+
+ll fact[300005];
+
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;      // Initialize result
+
+    x = x % p;  // Update x if it is more than or
+                // equal to p
+
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1)
+            res = (res*x) % p;
+
+        // y must be even now
+        y = y>>1; // y = y/2
+        x = (x*x) % p;
+    }
+    return res;
+}
+
+
+ll _nCr(ll n, ll r)
+{
+    if(r > n) return 0;
+
+    ll z = ((power(fact[r], mod - 2, mod)) * (power(fact[n - r], mod - 2, mod))) % mod;
+    return (fact[n] * z) % mod;
+}
+
+void precalc()
+{
+    fact[0] = 1;
+
+    for(ll i = 1; i <= 300000; i++)
+    {
+        fact[i] = (i * fact[i - 1]) % mod;
+    }
+}
