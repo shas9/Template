bc07da3d5935fb6f2a45af234b09909e7515cebb
shas9 <53976819+shas9@users.noreply.github.com>
2020-04-19 16:22:27 +0600

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,40 +1,3 @@
-#include <bits/stdc++.h>`
-
-#define slld(longvalue) scanf("%lld", &longvalue)
-#define plld(longvalue) printf("%lld\n", longvalue)
-
-#define slf(longvalue) scanf("%lf", &longvalue)
-#define plf(longvalue) printf("%lf\n", longvalue)
-#define sc(letter) scanf("%c", &letter)
-#define pc(letter) printf("%c", letter)
-
-#define ss(name) scanf("%s", name)
-#define ps(name) printf("%s", name)
-
-#define pnew printf("\n")
-
-#define ll long long
-
-#define printcase(indexing,ans) printf("Case %lld: %lld\n", indexing, ans)
-
-#define pb(x) push_back(x)
-
-#define bug printf("BUG\n")
-
-#define mxlld 2e18
-#define mnlld -2e18
-
-#define mxd 2e8
-#define mnd -2e8
-
-#define pi 3.14159265359
-
-#define mod 1000000009
-
-
-
-using namespace std;
-
 const ll maxn  = 1000002;
 vector < ll > primes;
 bool vis[maxn+10];
@@ -65,28 +28,3 @@ void sieve()
     }
 
 }
-
-int main()
-{
-    ll i, j, k, l, m, n, o, x;
-    ll testcase;
-    ll input, flag, tag, ans;
-
-    sieve();
-
-    // freopen("in.txt", "r", stdin);
-    slld(testcase);
-
-    for(ll cs = 1; cs <= testcase; cs++)
-    {
-        slld(x);
-        slld(k);
-        slld(m);
-
-        ll ephi = phi(m);
-    }
-
-
-}
-
-
