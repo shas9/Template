75bc714f88310fdd08142e695a73f383fce68c0f
shas9 <53976819+shas9@users.noreply.github.com>
2020-04-28 11:49:21 +0600

    Update and rename ncr.cpp to NCR.cpp

--- NCR.cpp
+++ NCR.cpp
@@ -1,25 +1,3 @@
-ll nCr(ll n, ll k )
-{
-    if (k > n) return 0;
-    if (k * 2 > n) k = n-k;
-    if (k == 0) return 1;
-
-//    ll result = n;
-//    for( int i = 2; i <= k; ++i ) {
-//        result *= (n-i+1);
-//        result /= i;
-//    }
-//
-//    return result;
-
-    ll x = 1, y = 1;
-
-    for(ll i = n - k + 1; i <= n; i++) x *= i, x %= mod;
-    for(ll i = 1; i <= k; i++) y *= i, y %= mod;
-
-    return (x / y) % mod;
-}
-
 ll fact[300005];
 
 ll power(ll x, ll y, ll p)
@@ -43,7 +21,7 @@ ll power(ll x, ll y, ll p)
 }
 
 
-ll _nCr(ll n, ll r)
+ll nCr(ll n, ll r)
 {
     if(r > n) return 0;
 
