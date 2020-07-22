b7fe8ea80aa507f6b063c90beb82aa4984ea91fb
shas9 <53976819+shas9@users.noreply.github.com>
2020-07-22 19:48:33 +0600

    Update Binary Indexed Tree.cpp

--- Binary Indexed Tree.cpp
+++ Binary Indexed Tree.cpp
@@ -1,18 +1,22 @@
-ll query(ll idx)
+struct BIT
 {
-       ll sum=0;
-       while(idx>0){
-             sum+=tree[idx];
-             idx -= idx & (-idx);
-       }
-       return sum;
-}
-
-void update(ll idx, ll x, ll n) //n is the size of the array, x is the number to add
-{
-       while(idx<=n)
+       ll tree[mx];
+       ll query(ll idx)
        {
-             tree[idx]+=x;
-             idx += idx & (-idx);
+              ll sum=0;
+              while(idx>0){
+                    sum+=tree[idx];
+                    idx -= idx & (-idx);
+              }
+              return sum;
+       }
+
+       void update(ll idx, ll x, ll n) //n is the size of the array, x is the number to add
+       {
+              while(idx<=n)
+              {
+                    tree[idx]+=x;
+                    idx += idx & (-idx);
+              }
        }
 }
