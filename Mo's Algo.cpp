475f9fe5bbc988c8e31bf6317dc9468d4efb5b4a
shas9 <53976819+shas9@users.noreply.github.com>
2021-01-09 13:37:39 +0600

    Update Mo's Algo.cpp

--- Mo's Algo.cpp
+++ Mo's Algo.cpp
@@ -2,17 +2,22 @@ struct node
 {
     ll l, r, id, blc;
 };
-
+ 
+const ll lim = 100005;
+ 
 node q[lim];
 ll ans[lim];
-
+ll freq[3000006];
+ll arr[lim];
+ll cnt;
+ 
 bool comp(node a, node b)
 {
     if(a.blc != b.blc)
     {
         return a.blc < b.blc;
     }
-
+ 
     if(a.blc % 2 == 0)
     {
         return a.r < b.r;
@@ -22,25 +27,33 @@ bool comp(node a, node b)
         return a.r > b.r;
     }
 }
-
+ 
 void add(ll x)
 {
-
+	ll need = arr[x] ^ k;
+ 
+	cnt += freq[need];
+ 
+	freq[arr[x]]++;
 }
-
+ 
 void rem(ll x)
 {
-
+	freq[arr[x]]--;
+ 
+	ll need = arr[x] ^ k;
+ 
+	cnt -= freq[need];
 }
-
-
+ 
+ 
 // q[i].blc = l / ssq;  /// block count
-
+ 
 /*
-
+	l = 1, r = 0, cnt = 0;
+ 
     while(l > q[i].l) add(--l);
     while(r < q[i].r) add(++r);
-    while(l < q[i].l) remove(l++);
-    while(r > q[i].r) remove(r--);
-
+    while(l < q[i].l) rem(l++);
+    while(r > q[i].r) rem(r--);
 */
