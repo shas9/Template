0fce0545bf94499adccb095c6f52a0c83d5ceb28
shas9 <53976819+shas9@users.noreply.github.com>
2021-03-31 18:22:05 +0600

    Update NCR.cpp

--- NCR.cpp
+++ NCR.cpp
@@ -20,65 +20,76 @@ ll nCr(ll n, ll r)
 // when mod is prime
 
 const ll sz = 300000;
-
+const ll mod = 998244353;
+ 
 ll fact[sz + 10];
-
+ll invfact[sz + 10];
+ 
 ll power(ll x, ll y, ll p)
 {
     ll res = 1;      // Initialize result
-
+ 
     x = x % p;  // Update x if it is more than or
                 // equal to p
-
+ 
     while (y > 0)
     {
         // If y is odd, multiply x with result
         if (y & 1)
             res = (res*x) % p;
-
+ 
         // y must be even now
         y = y>>1; // y = y/2
         x = (x*x) % p;
     }
     return res;
 }
-
-
+ 
+ 
 ll nCr(ll n, ll r)
 {
     if(r > n) return 0;
-
+ 
     if(n < mod)
     {
-        ll z = ((power(fact[r], mod - 2, mod)) * (power(fact[n - r], mod - 2, mod))) % mod;
+        ll z = (invfact[r] * invfact[n - r]) % mod;
         return (fact[n] * z) % mod;
     }
-    
+ 
     /// Lucas Theorem
     vector < ll > N, K;
-    
+ 
     while(n) N.push_back(n % mod), n /= mod;
-    
+ 
     while(r) K.push_back(r % mod), r /= mod;
-    
+ 
     ll ret = 1;
-    
+ 
     for(ll i = 0; i < K.size(); i++)
     {
         ret = (ret * nCr(N[i],K[i])) % mod;
     }
-    
+ 
     return ret;
 }
-
+ 
 void precalc()
 {
     fact[0] = 1;
-
+ 
     for(ll i = 1; i <= sz; i++)
     {
         fact[i] = (i * fact[i - 1]) % mod;
     }
+ 
+    invfact[sz] = power(fact[sz], mod - 2, mod);
+ 
+    for(ll i = sz - 1; i >= 0; i--)
+    {
+        invfact[i] = (invfact[i + 1] * (i + 1)) % mod;
+    }
+ 
+    return;
 }
 
 
