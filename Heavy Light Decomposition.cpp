a55eb88b12dfe399f9c8e30949e6e6bd0599d46d
shas9 <53976819+shas9@users.noreply.github.com>
2021-04-16 19:24:31 +0600

    Update Heavy Light Decomposition.cpp

--- Heavy Light Decomposition.cpp
+++ Heavy Light Decomposition.cpp
@@ -1,3 +1,73 @@
+struct segtree
+{
+
+    ll n;
+    vector < ll > tree;
+
+
+    void init(ll _n)
+    {
+        n = _n;
+        tree.assign(4 * n,0);
+    }
+
+    void treeupdate(ll node, ll b, ll e, ll i, ll val)
+    {
+        if(i > e || i < b)
+        {
+            return;
+        }
+
+        if(b == i && e == i)
+        {
+            tree[node] = val;
+            return;
+        }
+
+        ll left = node * 2;
+        ll right = (node * 2) + 1;
+        ll mid = (b + e) / 2;
+
+        treeupdate(left, b, mid, i, val);
+        treeupdate(right, mid + 1, e, i, val);
+
+        tree[node] = (tree[left] + tree[right]);
+    }
+
+    void update(ll i, ll val)
+    {
+        treeupdate(1,1,n,i,val);
+    }
+
+    ll treequery(ll node, ll b, ll e, ll i, ll j)
+    {
+        if(i > e || j < b)
+        {
+            return 0;
+        }
+
+        if(b >= i && e <= j)
+        {
+            return tree[node];
+        }
+
+        ll left = node * 2;
+        ll right = (node * 2) + 1;
+        ll mid = (b + e) / 2;
+
+        ll p1 = treequery(left, b, mid, i, j);
+        ll p2 = treequery(right, mid + 1, e,i, j);
+
+        return (p1 + p2);
+    }
+
+    ll query(ll x, ll y)
+    {
+        return treequery(1,1,n,x,y);
+    }
+
+} seg;
+
 /// Template: HLD
 
 /* DFS for Heavy-Light Decomposition
