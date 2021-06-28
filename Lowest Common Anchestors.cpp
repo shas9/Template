b12771dad3e01b9ae8487a5eb0751373f64ba059
shas9 <53976819+shas9@users.noreply.github.com>
2021-06-28 11:54:51 +0600

    Update Lowest Common Anchestors.cpp

--- Lowest Common Anchestors.cpp
+++ Lowest Common Anchestors.cpp
@@ -1,92 +1,52 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define ll long long
-bool check(ll N,ll pos)
-{
-    return (bool)(N & (1LL<<pos));
-}
-vector<ll>graph[100005];
-ll par[19][100005];
-ll depth[100005];
-ll pwr[20];
-ll n;
-void dfs0(ll src,ll p,ll level)
-{
-    depth[src]=level;
-    par[0][src]=p;
-    for(ll i=0; i<graph[src].size(); i++)
-    {
-        ll adj=graph[src][i];
-        if(adj!=p)
-        {
-            dfs0(adj,src,level+1);
-        }
+const ll K = 10;
+const ll N = 203;
+
+ll anc[N][K];
+ll depth[N];
+
+void setup(ll u, ll par) {
+    depth[u] = depth[par]+1;
+
+    memset(anc[u],0,sizeof anc[u]);
+
+    anc[u][0] = par;
+
+    for (ll k=1; k<K; k++) anc[u][k] = anc[anc[u][k-1]][k-1];
+
+    for (ll v: g[u]) {
+        if (v == par)   continue;
+        setup(v, u);
     }
 }
-void preL()
-{
- 
-    for(ll i=1; i<=17; i++)
-    {
-        for(ll j=1; j<=n; j++)
-        {
-            if(par[i-1][j]==-1)continue;
-            par[i][j]=par[i-1][par[i-1][j]];                       ///i means 2^i th parent of j th node par[i][j] means 2^i th parent of jth node
-        }
-    }
+
+ll lca(ll u, ll v) {
+
+    if (depth[u] > depth[v])    swap(u, v);
+
+    for (ll k=K-1; k>=0; k--)
+        if (depth[u] + (1<<k) <= depth[v])  v = anc[v][k];
+
+    if (u == v) return u;
+
+    for (ll k=K-1; k>=0; k--)
+        if (anc[u][k] != anc[v][k])
+            u = anc[u][k], v = anc[v][k];
+
+    return anc[u][0];
 }
-ll lca(ll u,ll v)
-{
-    if(depth[u]>depth[v])
-    {
-        swap(u,v);
-    }
-    ll diff=depth[v]-depth[u];
-    for(ll i=0; i<=17; i++)
-    {
-        if(check(diff,i)==1)
-        {
-            v=par[i][v];
-        }
-    }
-    if(u==v)
-    {
-        return u;
-    }
-    for(ll i=17; i>=0; i--)
-    {
-        if(par[i][u]!=par[i][v])
-        {
-            u=par[i][u];
-            v=par[i][v];
-        }
-    }
-    return par[0][u];
+
+ll getanc(ll u, ll d) {
+
+    for (ll k=0; k<K; k++)
+        if (d & (1<<k))
+            u = anc[u][k];
+
+    return u;
 }
-int main()
-{
-    memset(par,-1,sizeof par);
-    cin>>n;
-    for(ll i=1; i<=n-1; i++)
-    {
-        ll u,v;
-        cin>>u;
-        cin>>v;
-        graph[u].push_back(v);
-        graph[v].push_back(u);
-    }
-    dfs0(1,-1,0);
-    preL();
- 
-    ll q;
-    cin>>q;
-    for(ll i=1; i<=q; i++)
-    {
-        ll u,v;
-        cin>>u;
-        cin>>v;
-        ll lcaa=lca(u,v);
-        cout<<lcaa<<endl;
-    }
-    return 0;
+
+ll dist(ll u, ll v) {
+
+    ll x = lca(u, v);
+
+    return depth[u] + depth[v] - 2*depth[x];
 }
